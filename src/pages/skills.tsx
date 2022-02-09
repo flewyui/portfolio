ed76b405941dac81b4ff88af987b4e1c477060cd
flewyui <flewyui@gmail.com>
2022-02-09 22:21:58 +0900

    add skills

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -5,47 +5,22 @@ const Skills = () => {
                 <h1 className="flex items-center text-[30px] font-bold text-[#535353]"> Skills</h1>
             </div>
             <div className="h-[85%]">
-                <table className="m-auto">
-                    <tbody>
-                        <tr>
-                            <th>Name:</th>
-                        </tr>
-                        <tr>
-                            <td className="text-center">
-                                flewyui
-                                <br />
-                                <br />
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>age:</th>
-                        </tr>
-                        <tr>
-                            <td className="text-center">
-                                28
-                                <br />
-                                <br />
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>Accounts: </th>
-                        </tr>
-                        <tr>
-                            <td className="text-center">
-                                github:{' '}
-                                <a href="https://github.com/flewyui" target="_blank" rel="noreferrer">
-                                    https://github.com/flewyui
-                                </a>
-                                <br />
-                                <br />
-                                Qiita:{' '}
-                                <a href="https://qiita.com/flewyui" target="_blank" rel="noreferrer">
-                                    https://qiita.com/flewyui
-                                </a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div className="flex flex-row">
+                    <div className="flex-1 bg-[#535353] opacity-50 text-center h-[80px] text-white p-2 m-1">HTML</div>
+                    <div className="flex-1 bg-[#535353] opacity-50 text-center h-[80px] text-white p-2 m-1">CSS</div>
+                    <div className="flex-1 bg-[#535353] opacity-50 text-center h-[80px] text-white p-2 m-1">
+                        JavaScript
+                    </div>
+                    <div className="flex-1 bg-[#535353] opacity-50 text-center h-[80px] text-white p-2 m-1">React</div>
+                </div>
+                <div className="flex flex-row">
+                    <div className="flex-1 bg-[#535353] opacity-50 text-center h-[80px] text-white p-2 m-1">PHP</div>
+                    <div className="flex-1 bg-[#535353] opacity-50 text-center h-[80px] text-white p-2 m-1">
+                        Laravel
+                    </div>
+                    <div className="flex-1 bg-[#535353] opacity-50 text-center h-[80px] text-white p-2 m-1">AWS</div>
+                    <div className="flex-1 bg-[#535353] opacity-50 text-center h-[80px] text-white p-2 m-1">Docker</div>
+                </div>
             </div>
         </>
     )
