6389fb01229e5edf654073a6dc987045d9da21ac
flewyui <flewyui@gmail.com>
2022-02-11 23:25:57 +0900

    update products

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 const Products = () => {
     return (
         <>
@@ -6,14 +8,44 @@ const Products = () => {
             </div>
             <div className="h-[85%] animate-fadeIn">
                 <div className="flex flex-row">
-                    <div className="flex-1 p-2 m-1 h-[240px] text-center border-2 border-[#535353]">
+                    <div className="flex-1 p-2 m-1 h-[300px] text-center border-2 border-[#535353]">
                         <h2 className="text-[#535353]">Todo App [TS/React]</h2>
+                        <div className="flex justify-center">
+                            <Image
+                                src="/images/cp-ts-react-todo.jpg"
+                                width={220}
+                                height={200}
+                                objectFit="contain"
+                                priority
+                                alt="TS Todo App"
+                            ></Image>
+                        </div>
                     </div>
-                    <div className="flex-1 p-2 m-1 h-[240px] text-center border-2 border-[#535353]">
+                    <div className="flex-1 p-2 m-1 h-[300px] text-center border-2 border-[#535353]">
                         <h2 className="text-[#535353]">tic-tac-toe [ReactHooks]</h2>
+                        <div className="flex justify-center">
+                            <Image
+                                src="/images/cp-tic-tac-toe.jpg"
+                                width={220}
+                                height={200}
+                                objectFit="contain"
+                                priority
+                                alt="tic-tac-toe"
+                            ></Image>
+                        </div>
                     </div>
-                    <div className="flex-1 p-2 m-1 h-[240px] text-center border-2 border-[#535353]">
+                    <div className="flex-1 p-2 m-1 h-[300px] text-center border-2 border-[#535353]">
                         <h2 className="text-[#535353]">Todo App [PHP/Laravel]</h2>
+                        <div className="flex justify-center">
+                            <Image
+                                src="/images/cp-laravel-todo.jpg"
+                                width={350}
+                                height={200}
+                                objectFit="contain"
+                                priority
+                                alt="Laravel Todo App"
+                            ></Image>
+                        </div>
                     </div>
                 </div>
             </div>
