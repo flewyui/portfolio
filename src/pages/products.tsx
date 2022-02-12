13047211f20e368e0d71388d4a7de323c805cd23
flewyui <flewyui@gmail.com>
2022-02-12 22:21:13 +0900

    fix img path

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -9,20 +9,30 @@ const Products = () => {
                     <div className="flex-1 p-2 m-1 h-[300px] text-center border-2 border-[#535353]">
                         <h2 className="text-[#535353]">Todo App [TS/React]</h2>
                         <div className="flex justify-center">
-                            <img src="/images/cp-ts-react-todo.jpg" width={220} height={200} alt="TS Todo App"></img>
+                            <img
+                                src="../../images/cp-ts-react-todo.jpg"
+                                width={220}
+                                height={200}
+                                alt="TS Todo App"
+                            ></img>
                         </div>
                     </div>
                     <div className="flex-1 p-2 m-1 h-[300px] text-center border-2 border-[#535353]">
                         <h2 className="text-[#535353]">tic-tac-toe [ReactHooks]</h2>
                         <div className="flex justify-center">
-                            <img src="/images/cp-tic-tac-toe.jpg" width={220} height={200} alt="tic-tac-toe"></img>
+                            <img
+                                src={'../../images/cp-tic-tac-toe.jpg'}
+                                width={220}
+                                height={200}
+                                alt="tic-tac-toe"
+                            ></img>
                         </div>
                     </div>
                     <div className="flex-1 p-2 m-1 h-[300px] text-center border-2 border-[#535353]">
                         <h2 className="text-[#535353]">Todo App [PHP/Laravel]</h2>
                         <div className="flex justify-center">
                             <img
-                                src="/images/cp-laravel-todo.jpg"
+                                src={'../../images/cp-laravel-todo.jpg'}
                                 width={350}
                                 height={200}
                                 alt="Laravel Todo App"
