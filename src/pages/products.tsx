725e21206ef617cbd88c26bee684f44439a71ab2
flewyui <flewyui@gmail.com>
2022-02-12 21:33:28 +0900

    change Image to img

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 const Products = () => {
     return (
         <>
@@ -11,40 +9,24 @@ const Products = () => {
                     <div className="flex-1 p-2 m-1 h-[300px] text-center border-2 border-[#535353]">
                         <h2 className="text-[#535353]">Todo App [TS/React]</h2>
                         <div className="flex justify-center">
-                            <Image
-                                src="/images/cp-ts-react-todo.jpg"
-                                width={220}
-                                height={200}
-                                objectFit="contain"
-                                priority
-                                alt="TS Todo App"
-                            ></Image>
+                            <img src="/images/cp-ts-react-todo.jpg" width={220} height={200} alt="TS Todo App"></img>
                         </div>
                     </div>
                     <div className="flex-1 p-2 m-1 h-[300px] text-center border-2 border-[#535353]">
                         <h2 className="text-[#535353]">tic-tac-toe [ReactHooks]</h2>
                         <div className="flex justify-center">
-                            <Image
-                                src="/images/cp-tic-tac-toe.jpg"
-                                width={220}
-                                height={200}
-                                objectFit="contain"
-                                priority
-                                alt="tic-tac-toe"
-                            ></Image>
+                            <img src="/images/cp-tic-tac-toe.jpg" width={220} height={200} alt="tic-tac-toe"></img>
                         </div>
                     </div>
                     <div className="flex-1 p-2 m-1 h-[300px] text-center border-2 border-[#535353]">
                         <h2 className="text-[#535353]">Todo App [PHP/Laravel]</h2>
                         <div className="flex justify-center">
-                            <Image
+                            <img
                                 src="/images/cp-laravel-todo.jpg"
                                 width={350}
                                 height={200}
-                                objectFit="contain"
-                                priority
                                 alt="Laravel Todo App"
-                            ></Image>
+                            ></img>
                         </div>
                     </div>
                 </div>
