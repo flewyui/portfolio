4d17d45d887e8bbf36a77899548f89a5dd17822b
flewyui <flewyui@gmail.com>
2022-02-13 22:56:31 +0900

    update history

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -4,48 +4,17 @@ const History = () => {
             <div className="flex justify-center h-[15%] animate-fadeIn">
                 <h1 className="flex items-center text-[30px] font-bold text-[#535353]"> History</h1>
             </div>
-            <div className="h-[85%] animate-fadeIn">
-                <table className="m-auto">
-                    <tbody>
-                        <tr>
-                            <th>Name:</th>
-                        </tr>
-                        <tr>
-                            <td className="text-center">
-                                flewyui
-                                <br />
-                                <br />
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>age:</th>
-                        </tr>
-                        <tr>
-                            <td className="text-center">
-                                28
-                                <br />
-                                <br />
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>Accounts: </th>
-                        </tr>
-                        <tr>
-                            <td className="text-center">
-                                github:{' '}
-                                <a href="https://github.com/flewyui" target="_blank" rel="noreferrer">
-                                    https://github.com/flewyui
-                                </a>
-                                <br />
-                                <br />
-                                Qiita:{' '}
-                                <a href="https://qiita.com/flewyui" target="_blank" rel="noreferrer">
-                                    https://qiita.com/flewyui
-                                </a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div className="h-[85%]  text-[#535353] text-center animate-fadeIn">
+                <h1 className="font-bold text-[#535353] my-5">プロジェクト参画歴</h1>
+                <p>2020年1月</p>
+                <p>ファッションECサイト開発</p>
+                <p className="mt-2">2020年12月</p>
+                <p>クレジットカードアプリ開発</p>
+                <h1 className="font-bold text-[#535353] my-5">職歴</h1>
+                <p>2015年4月</p>
+                <p>株式会社帝国ホテル入社</p>
+                <p className="mt-2">2019年8月</p>
+                <p>株式会社Carecon入社</p>
             </div>
         </>
     )
